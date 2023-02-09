32de6bd42aeae53fd49a9b3be437ef5d915ce27e
Andrew S <samum_94@mail.ru>
2023-02-09 19:55:45 +0300

    Application created

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,18 @@ public class Main {
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
-
+        int originalBalance = 123;
+        int replenishment = 1500;
+        int sumBalance;
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+        if (replenishment > 1000) {
+            sumBalance = originalBalance + replenishment + replenishment / 100;
+        } else {
+            sumBalance = originalBalance + replenishment;
+        }
+        System.out.println("Итоговый баланс: " + sumBalance + " рублей");
     }
 }
